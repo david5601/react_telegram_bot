6bb464d52d146423897aff53d57c475962bfc900
vamp29466 <vamp29466@gmail.com>
2024-07-18 10:48:34 +0530

    push

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import "./App.css";
 import React, { useEffect } from "react";
 
+window.Telegram.WebApp.MainButton.show();
+window.Telegram.WebApp.MainButton.setText("Hello");
+window.Telegram.WebApp.MainButton.onClick(() => {
+  window.Telegram.WebApp.showAlert("Hello World");
+});
+
 function App() {
-  // useEffect(() => {
-  //   if (window.Telegram && window.Telegram.WebApp) {
-  //     window.Telegram.WebApp.ready();
-  //     // console.log("Hello World!");
-  //     // window.Telegram.WebApp.showAlert("Hello World");
-  //   }
-  //   // console.log("No Hello World");
-  // }, []);
+  useEffect(() => {
+    if (window.Telegram && window.Telegram.WebApp) {
+      window.Telegram.WebApp.ready();
+      // console.log("Hello World!");
+      // window.Telegram.WebApp.showAlert("Hello World");
+    }
+    // console.log("No Hello World");
+  }, []);
 
   return (
     <>
