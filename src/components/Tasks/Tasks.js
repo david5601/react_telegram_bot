007465ea83c7d46c88d847d8e5438dc40ed7ebc1
vamp29466 <vamp29466@gmail.com>
2024-08-06 08:50:38 +0530

    push

--- src/components/Tasks/Tasks.js
+++ src/components/Tasks/Tasks.js
@@ -34,7 +34,7 @@ const Tasks = () => {
         {initialzeTasks?.map((item, index) => {
           console.log(item)
           return (
-              item.task_completed_status ? 
+              isChecked ? 
               <TaskComElement/> : <TaskElement title={item.task_name} bonus={item.bonus} during={"30"} state={item.task_completed_status} key={0} link={item.link}/>
             // <TaskElement icon={item.image_url} title={item.task_name} bonus={item.bonus} during={"30"} state={item.task_completed_status} key={index} />
           )
